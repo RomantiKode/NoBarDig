61ba3fd8fd7000fcd48f050c60af26e65bac6801
mj1Per127 <310484852+mj1Per127@users.noreply.github.com>
2026-07-31 01:27:05 +0700

    Temporarily remove Dubbindo

--- Dubbindo/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-version = 3
-cloudstream {
-    language = "id"
-    authors = listOf("ByAyzen", "Hexated", "TeKuma25")
-    description = "Dubbindo v7: kompatibel CloudStream stable, kategori lengkap, Shorts, multi-quality, embed fallback, dan subtitle"
-    status = 1
-    tvTypes = listOf("Movie", "AnimeMovie")
-    iconUrl = "https://www.dubbindo.site/themes/dubbindo/img/icon.png"
-}
