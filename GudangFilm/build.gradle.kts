c96c7f490ce67ac8caea64f5a8ce8fe2815bbb40
mj1Per127 <310484852+mj1Per127@users.noreply.github.com>
2026-08-04 01:23:12 +0700

    Tambah GudangFilm plugin

---
+++ GudangFilm/build.gradle.kts
@@ -0,0 +1,14 @@
+version = 3
+
+cloudstream {
+    language = "id"
+    description = "GudangFilm Adalah Website Nonton Film Streaming Movie Layarkaca21 Lk21 Dunia21 Bioskop Cinema 21 Box Office Subtitle Indonesia Gratis Online"
+    authors = listOf("Agoose")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://www.huazai6.com/wp-content/uploads/2026/07/images.jpg"
+}
+
+android {
+    namespace = "com.mts.gudangfilm"
+}
