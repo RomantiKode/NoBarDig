476b4f9a1ff3e6157c2ea9bce957175bcbc00a72
mj1Per127 <310484852+mj1Per127@users.noreply.github.com>
2026-08-12 05:58:11 +0700

    Hapus semua modul Cloudstream

--- Juraganfilm/src/main/kotlin/com/juraganfilm/Extractors.kt
+++
@@ -1,4 +0,0 @@
-package com.juraganfilm
-
-// Extractor khusus belum diperlukan. Provider mengambil tautan direct dari HTML
-// dan memakai loadExtractor bawaan Cloudstream sebagai fallback untuk host lain.
