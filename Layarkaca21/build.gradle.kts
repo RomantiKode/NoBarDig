554b282a5a312b085565f05173c4fc7a78791db0
mj1Per127 <310484852+mj1Per127@users.noreply.github.com>
2026-08-19 09:16:50 +0700

    Update Layarkaca21 V9

--- Layarkaca21/build.gradle.kts
+++ Layarkaca21/build.gradle.kts
@@ -1,7 +1,7 @@
 // CANONICAL/READABLE SOURCE BUILD FILE — Agoose Modular v3r4r13
 import java.util.Properties
 
-version = 8
+version = 9
 val localProperties = Properties().apply {
     val file = rootProject.file("local.properties")
     if (file.isFile) file.inputStream().use(::load)
